f3087eb3a8a84dd6ec42234faa2e4d838774e917
shadrack9 <shadrackdanford@hotmail.com>
2020-05-15 17:40:45 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import dash
+import dash_table
+import pandas as pd
+
+df = pd.read_csv('tweet-gigs.csv')
+
+app = dash.Dash(__name__)
+
+app.layout = dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df.columns],
+    style_cell={'textAlign': 'left'},
+    style_header={
+        'backgroundColor': 'rgb(230, 230, 230)',
+        'fontWeight': 'bold',
+        'border': '1px solid pink'
+    },
+    style_data={ 'border': '1px solid blue',
+                 'whiteSpace': 'normal',
+                 'height': 'auto'
+                  },
+    data=df.to_dict('records'),
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
